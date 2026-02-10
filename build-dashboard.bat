1a453e2d023a84f1fe5c245df0a42f59421eee1c
Joel Christner <joel.christner@gmail.com>
2026-02-09 21:26:24 -0800

    Docker

--- build-dashboard.bat
+++ build-dashboard.bat
@@ -2,7 +2,7 @@
 IF "%1" == "" GOTO :Usage
 ECHO.
 ECHO Building DocumentAtom Dashboard for linux/amd64 and linux/arm64/v8...
-docker buildx build -f dashboard/Dockerfile --builder cloud-jchristn77-jchristn77 --no-cache --platform linux/amd64,linux/arm64/v8 --tag jchristn77/documentatom-ui:%1 --tag jchristn77/documentatom-ui:latest --push .
+docker buildx build -f dashboard/Dockerfile --builder cloud-jchristn77-jchristn77 --platform linux/amd64,linux/arm64/v8 --tag jchristn77/documentatom-ui:%1 --tag jchristn77/documentatom-ui:latest --push .
 GOTO :Done
 
 :Usage
